697793428368b804f5bbffd33b651d40c912b196
liverday <vitor.medeiro10@gmail.com>
2021-03-14 20:11:30 -0300

    refactor: tornando sidebar fixed no mobile, configurando corretamente pwa

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -17,31 +17,31 @@ export default class MyDocument extends Document {
                     <meta name='description' content='Produtividade anti sedentarismo' />
                     <meta name='format-detection' content='telephone=no' />
                     <meta name='mobile-web-app-capable' content='yes' />
-                    <meta name='msapplication-config' content='/static/icons/browserconfig.xml' />
+                    <meta name='msapplication-config' content='/icons/browserconfig.xml' />
                     <meta name='msapplication-TileColor' content='#2B5797' />
                     <meta name='msapplication-tap-highlight' content='no' />
                     <meta name='theme-color' content='#000000' />
 
-                    <link rel='apple-touch-icon' sizes='180x180' href='/static/icons/apple-touch-icon.png' />
-                    <link rel='icon' type='image/png' sizes='32x32' href='/static/icons/favicon-32x32.png' />
-                    <link rel='icon' type='image/png' sizes='16x16' href='/static/icons/favicon-16x16.png' />
-                    <link rel='manifest' href='/static/manifest.json' />
-                    <link rel='mask-icon' href='/static/icons/safari-pinned-tab.svg' color='#5bbad5' />
-                    <link rel='shortcut icon' href='/static/icons/favicon.ico' />
+                    <link rel='apple-touch-icon' sizes='180x180' href='/icons/apple-touch-icon.png' />
+                    <link rel='icon' type='image/png' sizes='32x32' href='/icons/favicon-32x32.png' />
+                    <link rel='icon' type='image/png' sizes='16x16' href='/icons/favicon-16x16.png' />
+                    <link rel='manifest' href='/manifest.json' />
+                    <link rel='mask-icon' href='/icons/safari-pinned-tab.svg' color='#5bbad5' />
+                    <link rel='shortcut icon' href='/icons/favicon.ico' />
                     <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500' />
 
                     <meta name='twitter:card' content='summary' />
                     <meta name='twitter:url' content='https://yourdomain.com' />
                     <meta name='twitter:title' content='move.it' />
                     <meta name='twitter:description' content='Produtividade anti sedentarismo' />
-                    <meta name='twitter:image' content='https://yourdomain.com/static/icons/android-chrome-192x192.png' />
+                    <meta name='twitter:image' content='https://yourdomain.com/icons/android-chrome-192x192.png' />
                     <meta name='twitter:creator' content='@DavidWShadow' />
                     <meta property='og:type' content='website' />
                     <meta property='og:title' content='move.it' />
                     <meta property='og:description' content='Produtividade anti sedentarismo' />
                     <meta property='og:site_name' content='move.it' />
                     <meta property='og:url' content='https://yourdomain.com' />
-                    <meta property='og:image' content='https://yourdomain.com/static/icons/apple-touch-icon.png' />
+                    <meta property='og:image' content='https://yourdomain.com/icons/apple-touch-icon.png' />
                 </Head>
                 <body>
                     <Main />
